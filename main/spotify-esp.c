f9919e6a8f1d679709b5278324a432ba9bfc8776
Suphakit P <me@suphakit.net>
2023-10-14 22:38:58 +0700

    init<all>: initial commit

---
+++ main/spotify-esp.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <WiFi.h>
+
+void app_main(void)
+{
+  Serial.begin(115200);
+  delay(1000);
+
+  WiFi.mode(WIFI_STA); //Optional
+  WiFi.begin(ssid, password);
+  Serial.println("\nConnecting");
+
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(100);
+  }
+
+  Serial.println("\nConnected to the WiFi network");
+  Serial.print("Local ESP32 IP: ");
+  Serial.println(WiFi.localIP());
+}
